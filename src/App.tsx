6f9b21756d9155830183941d771d4fffeb45b47f
KimYoungBeom <tiger_94@team-plat.com>
2024-11-20 23:27:39 +0900

    폰트 수정 및 헤더 네비게이션 추가중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Header from "@/components/layout/Header";
 import Home from "@/pages/Home";
+import Header from "@/components/layout/Header";
 
 function App() {
     return (
